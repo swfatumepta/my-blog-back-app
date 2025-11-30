ba582a5744f5c95cb07a2a1716377fa122b84ddd
ftmpt <fatumepta-git@yandex.ru>
2025-11-30 21:43:11 +0300

    [ITERATION-2] migration from JdbcTemplate to NamedParameterJdbcTemplate - TagJdbcRepository

--- src/main/java/edu/yandex/project/repository/jdbc/TagJdbcRepository.java
+++ src/main/java/edu/yandex/project/repository/jdbc/TagJdbcRepository.java
@@ -5,30 +5,30 @@ import edu.yandex.project.repository.TagRepository;
 import edu.yandex.project.repository.jdbc.util.DbUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
+@Repository
 @RequiredArgsConstructor
 @Slf4j
-@Repository
 public class TagJdbcRepository implements TagRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public List<TagEntity> createPostTags(@NonNull Long postId, @NonNull List<String> tagsToBeAdded) {
         log.debug("TagJdbcRepository::addPostTags post.id = {}, tags = {} in", postId, tagsToBeAdded);
         List<TagEntity> addedTagEntities = List.of();
         if (!tagsToBeAdded.isEmpty()) {
-            var tagsToBeAddedTextArray = DbUtil.convertToNativePostgreSqlTextArray(jdbcTemplate, tagsToBeAdded);
             var sql = """
-                        WITH given_tags(t_name) AS (SELECT UNNEST(?::text[])),
+                        WITH given_tags(t_name) AS (SELECT UNNEST(:givenTags::text[])),
                         inserted_tags AS (
                             INSERT INTO tags (name)
                                 SELECT t_name
@@ -46,7 +46,7 @@ public class TagJdbcRepository implements TagRepository {
                         ),
                         linked_tags AS (
                             INSERT INTO post_tag (post_id, tag_id)
-                                SELECT ?, id FROM all_tags
+                                SELECT :postId, id FROM all_tags
                                 ON CONFLICT (post_id, tag_id) DO NOTHING
                                 RETURNING tag_id
                         )
@@ -55,7 +55,15 @@ public class TagJdbcRepository implements TagRepository {
                     WHERE at.id IN (SELECT tag_id FROM linked_tags)
                     ORDER BY at.name;
                     """;
-            addedTagEntities = jdbcTemplate.query(sql, new TagEntityRowMapper(), tagsToBeAddedTextArray, postId);
+
+            var tagsToBeAddedTextArray = DbUtil.convertToNativePostgreSqlTextArray(
+                    namedParameterJdbcTemplate.getJdbcTemplate(), tagsToBeAdded
+            );
+            var namedParameters = Map.of(
+                    "postId", postId,
+                    "givenTags", tagsToBeAddedTextArray
+            );
+            addedTagEntities = namedParameterJdbcTemplate.query(sql, namedParameters, new TagEntityRowMapper());
         }
         log.debug("TagJdbcRepository::addPostTags post.id = {}, tags = {} out", postId, addedTagEntities);
         return addedTagEntities;
@@ -68,9 +76,9 @@ public class TagJdbcRepository implements TagRepository {
                 SELECT t.id, t.name, t.created_at
                 FROM tags t
                     LEFT JOIN post_tag pt ON t.id = pt.tag_id
-                WHERE pt.post_id = ?
+                WHERE pt.post_id = :postId
                 """;
-        var tagEntities = jdbcTemplate.query(sql, new TagEntityRowMapper(), postId);
+        var tagEntities = namedParameterJdbcTemplate.query(sql, Map.of("postId", postId), new TagEntityRowMapper());
         log.debug("TagJdbcRepository::addPostTags {} out. Result: {}", postId, tagEntities);
         return tagEntities;
     }
@@ -78,8 +86,8 @@ public class TagJdbcRepository implements TagRepository {
     @Override
     public void unlinkAllTagsFromPost(@NonNull Long postId) {
         log.debug("TagJdbcRepository::unlinkAllTagsFromPost {} in", postId);
-        var sql = "DELETE FROM post_tag WHERE post_id = ?";
-        jdbcTemplate.update(sql, postId);
+        var sql = "DELETE FROM post_tag WHERE post_id = :postId";
+        namedParameterJdbcTemplate.update(sql, Map.of("postId", postId));
         log.debug("TagJdbcRepository::addPostTags {} unlinkAllTagsFromPost out", postId);
     }
 
